06a19ebc6f9426012439fdbf85bacc2995319362
Grokberg <ruben.ne.svensson@gmail.com>
2025-11-07 21:51:31 +0100

    refactor index.php to remove unused database test link and streamline action imports

--- api/public/index.php
+++ api/public/index.php
@@ -1,12 +1,13 @@
 <?php declare(strict_types=1);
 
-use App\Database;
+use App\Action\ListWorkplaceAction;
+use App\Action\ListTimeReportAction;
+use App\Action\CreateTimeReportAction;
 use Slim\Factory\AppFactory;
 
 $container = require __DIR__ . '/../src/bootstrap.php';
 
 $app = AppFactory::createFromContainer($container);
-
 $app->addBodyParsingMiddleware();
 
 $errorMiddleware = $app->addErrorMiddleware(
@@ -15,15 +16,8 @@ $errorMiddleware = $app->addErrorMiddleware(
     logErrorDetails: true
 );
 
-$app->get('/api/workplace', \App\Action\ListWorkplaceAction::class);
-$app->get('/api/timereport[/{id}]', \App\Action\ListTimeReportAction::class);
-$app->post('/api/timereport', \App\Action\CreateTimeReportAction::class);
-$app->get('/testlink', function ($request, $response) {
-    $db = $this->get(Database::class);
-    $db->linkImageToReport(1, 'test.jpg');
-
-    $response->getBody()->write("Test link is working!");
-    return $response;
-});
+$app->get('/api/workplace', ListWorkplaceAction::class);
+$app->get('/api/timereport[/{id}]', ListTimeReportAction::class);
+$app->post('/api/timereport', CreateTimeReportAction::class);
 
 $app->run();
