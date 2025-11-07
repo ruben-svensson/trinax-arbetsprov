49f48272273761dcc57182c7e1092137e753f34b
Grokberg <ruben.ne.svensson@gmail.com>
2025-11-07 16:46:05 +0100

    moved options

---
+++ api/src/Options/TimeReportFilterOptions.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+namespace App\Options;
+
+use DateTimeImmutable;
+
+final class TimeReportFilterOptions {
+    public function __construct(
+        public readonly ?int $workplaceId = null,
+        public readonly ?DateTimeImmutable $fromDate = null,
+        public readonly ?DateTimeImmutable $toDate = null
+    ) {}
+
+    public function toQueryParams(): array {
+        return array_filter([
+            'workplaceId' => $this->workplaceId,
+            'from_date'    => $this->fromDate ? $this->fromDate->format('Y-m-d') : null,
+            'to_date'      => $this->toDate ? $this->toDate->format('Y-m-d') : null,
+        ]);
+    }
+
+    public static function fromArray(string $query): self {
+        parse_str($query, $params);
+
+        return new self(
+            workplaceId: isset($params['workplaceId']) ? (int)$params['workplaceId'] : null,
+            fromDate: isset($params['from_date']) ? new DateTimeImmutable($params['from_date']) : null,
+            toDate: isset($params['to_date']) ? new DateTimeImmutable($params['to_date']) : null,
+        );
+    }
+}
