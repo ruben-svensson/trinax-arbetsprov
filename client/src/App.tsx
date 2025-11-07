c08300e881cca619c4af4be923df9a048937b3fa
Grokberg <ruben.ne.svensson@gmail.com>
2025-11-07 16:41:57 +0100

    added frontend/react client concerns

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import TimeReportTable from './components/TimeReportTable'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <TimeReportTable />
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/TimeReportTable.tsx
@@ -0,0 +1,28 @@
+
+function TimeReportTable() {
+    return (
+        <table>
+        <thead>
+          <tr>
+            <th>Datum</th>
+            <th>Arbetsplatsnamn</th>
+            <th>Timmar</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>2024-01-01</td>
+            <td>Företag AB</td>
+            <td>8</td>
+          </tr>
+          <tr>
+            <td>2024-01-02</td>
+            <td>Exempel Företag</td>
+            <td>6</td>
+          </tr>
+        </tbody>
+      </table>
+    )
+}
+
+export default TimeReportTable
