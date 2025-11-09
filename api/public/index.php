d7ec22db9486dfe21194299eda6bc83cd44898b4
Grokberg <ruben.ne.svensson@gmail.com>
2025-11-09 12:26:55 +0100

    fix: add CORS headers for development environment

--- api/public/index.php
+++ api/public/index.php
@@ -8,6 +8,16 @@ use Slim\Factory\AppFactory;
 $container = require __DIR__ . '/../src/bootstrap.php';
 
 $app = AppFactory::createFromContainer($container);
+
+// Fix cors to work in development
+$app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
 $app->addBodyParsingMiddleware();
 
 $errorMiddleware = $app->addErrorMiddleware(
