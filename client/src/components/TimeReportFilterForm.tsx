79d4f7707d4e9c7d0b5895da311fb7142a7f749d
Grokberg <ruben.ne.svensson@gmail.com>
2025-11-09 16:26:45 +0100

    feat: add TimeReportFilterForm component for filtering time reports

---
+++ client/src/components/TimeReportFilterForm.tsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import type { TimeReportFilters, Workplace } from "../types";
+import FormGroup from "./FormGroup/FormGroup";
+
+interface TimeReportFilterFormProps {
+  workplaces: Workplace[];
+  onFilterChange?: (filters: TimeReportFilters) => void;
+}
+
+function TimeReportFilterForm({ workplaces, onFilterChange }: TimeReportFilterFormProps) {
+    const [workplace, setWorkplace] = useState<number | null>(null);
+    const [fromDate, setFromDate] = useState<string>('');
+    const [toDate, setToDate] = useState<string>('');
+
+    const handleWorkplaceChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const value = e.target.value ? Number(e.target.value) : null;
+        setWorkplace(value);
+        onFilterChange?.({ workplaceId: value, fromDate, toDate });
+    }
+
+    const handleFromDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        setFromDate(value);
+        onFilterChange?.({ workplaceId: workplace, fromDate: value, toDate });
+    }
+
+    const handleToDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        setToDate(value);
+        onFilterChange?.({ workplaceId: workplace, fromDate, toDate: value });
+    }
+
+    return (
+       <form className="form-stack">
+        <FormGroup label="Välj arbetsplats:" htmlFor="workplace-select">
+          <select 
+            id="workplace-select" 
+            name="workplace" 
+            value={workplace ?? ''} 
+            onChange={handleWorkplaceChange}
+          >
+            <option value="">--Alla--</option>
+            {workplaces.map(wp => (
+              <option key={wp.id} value={wp.id}>
+                {wp.name}
+              </option>
+            ))}
+          </select>
+        </FormGroup>
+        <FormGroup label="Från:" htmlFor="from-date">
+          <input 
+            type="date" 
+            id="from-date" 
+            name="fromDate" 
+            value={fromDate} 
+            onChange={handleFromDateChange} 
+          />
+        </FormGroup>
+        <FormGroup label="Till:" htmlFor="to-date">
+          <input 
+            type="date" 
+            id="to-date" 
+            name="toDate" 
+            value={toDate} 
+            onChange={handleToDateChange} 
+          />
+        </FormGroup>
+       </form>
+    )
+}
+
+export default TimeReportFilterForm
