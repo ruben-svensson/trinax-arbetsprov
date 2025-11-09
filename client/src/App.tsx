9e840002145156e49d0ee1569e537c167aa7b7b8
Grokberg <ruben.ne.svensson@gmail.com>
2025-11-09 16:25:20 +0100

    feat: implement time reporting features with filtering and creation forms

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,58 @@
 import './App.css'
+import TimeReportFilterForm from './components/TimeReportFilterForm'
+import TimeReportCreateForm from './components/TimeReportCreateForm'
 import TimeReportTable from './components/TimeReportTable'
+import useTimeReports from './hooks/useTimeReports';
+import useWorkplaces from './hooks/useWorkplaces';
+import { useMemo, useState } from 'react';
+import type { TimeReportFilters } from './types';
 
 function App() {
+  const [filters, setFilters] = useState<TimeReportFilters>({});
+  
+  const { timeReports, refreshTimeReports, isLoading } = useTimeReports(filters);
+  const { workplaces } = useWorkplaces();
+
+  const workplacesMap = useMemo(() => {
+    return new Map(workplaces.map(wp => [wp.id, wp]));
+  }, [workplaces]);
 
   return (
-    <>
-      <h1>Hello</h1>
-      <TimeReportTable />
-    </>
-  )
+    <main className="app">
+      <header className="header">
+        <h1>Tidrapportering</h1>
+      </header>
+      <div className="layout">
+        <div className="column">
+          <section>
+            <h2>Filtrera rapporter</h2>
+            <TimeReportFilterForm 
+              workplaces={workplaces} 
+              onFilterChange={setFilters} 
+            />
+          </section>
+          <section>
+            <h2>Befintliga rapporter</h2>
+            {isLoading ? (
+              <p>Laddar...</p>
+            ) : (
+              <TimeReportTable 
+                timeReports={timeReports} 
+                workplacesMap={workplacesMap} 
+              />
+            )}
+          </section>
+        </div>
+
+        <div className="column">
+          <section>
+            <h2>Skapa ny rapport</h2>
+            <TimeReportCreateForm onReportCreated={refreshTimeReports}/>
+          </section>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App
